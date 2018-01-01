a0291ca96c47d76526137e87c76b9f5f3c9bf9ec
OneStig <therealonestig@gmail.com>
2017-12-31 20:43:43 -0800

    Steam replica

--- bootstrap-testing/main.js
+++ bootstrap-testing/main.js
@@ -1,3 +1,6 @@
+var colors = ['#000000', '#FFFFFF', '#FF0000', '#FF6A00', '#FFD800', '#00FF00', '#0000FF', '#4800FF'];
+// colors.length = 8, last is 7
+var ccolor = 0;
 
 function refresh() {
   window.location = window.location;
@@ -9,4 +12,38 @@ function search() {
     searchItem = "https://www.google.com/search?q=" + searchItem;
     window.location = searchItem;
   }
+}
+
+function randomColor() {
+  var base16 = "0123456789ABCDEF";
+  var hex = "";
+
+  for (var i = 0; i < 6; i++) {
+    var r = Math.floor(Math.random() * 16);
+    hex += base16[r];
+  }
+
+  hex = "#" + hex;
+
+  setColor(hex);
+}
+
+function cycleColors() {
+  if (ccolor == colors.length - 1) {
+    ccolor = 0;
+  } else {
+    ccolor ++;
+  }
+
+  setColor(colors[ccolor]);
+}
+
+function setColor(hexColor) {
+  var allHeaders = document.querySelectorAll("h1");
+
+  for (var i = 0; i < allHeaders.length; i++) {
+    allHeaders[i].style.color = hexColor;
+  }
+
+  document.getElementById("currentColor").innerHTML = "Current Hex: " + hexColor;
 }
